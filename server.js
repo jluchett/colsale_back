1f4b7c3776652378a22853440146cda75c73630c
jluchett <jluchett@hotmail.com>
2024-11-02 23:26:11 -0500

    Agregado ruta pra auth

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const db = require('./src/database/db_conn')
+
+const authRouter = require('./src/routes/auth/authRoutes')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/test', async (req, res) => {
-  try {
-      const result = await db.query('SELECT NOW()');
-      res.json({ message: 'Conexión exitosa', time: result.rows[0].now });
-  } catch (error) {
-      console.error('Error en la conexión', error);
-      res.status(500).json({ message: 'Error en la conexión a la base de datos' });
-  }
-});
+app.use('/auth', authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -25,6 +25,4 @@ async function login(req, res) {
     }
 }
 
-module.exports = {
-    login
-};
+module.exports = login;

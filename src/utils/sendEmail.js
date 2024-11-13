36b6396c2768cb1b0c2cd9e44fb8b290b998c000
jluchett <jluchett@hotmail.com>
2024-11-13 15:29:09 -0500

    Cambiado cliente de email a google

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,21 +1,20 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.REMITE,
-      pass: process.env.CONTRA,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.GMAIL_USER,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN
+  }
   });
 
 const sendVerificationEmail = async (email, code, nombre) => {
    const mailOptions = {
-      from: process.env.REMITE,
+      from: process.env.GMAIL_USER,
       to: email,
       subject: 'Código de verificación',
       text: `Hola ${nombre},\n\nUtiliza el siguiente codigo para activar tu usuario e iniciar sesion:\n\n${code}`
